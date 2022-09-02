0698c408208b1f13360c5f2cee2ccd1ece918fb0
zqqf16 <zqqf16@gmai.com>
2022-09-02 19:26:31 +0800

    Merge files

--- Monster/Model/Entrance.swift
+++ Monster/Model/Entrance.swift
@@ -8,3 +8,51 @@
 import Foundation
 import AppKit
 
+enum Entrance: Int, CaseIterable, Identifiable {
+    
+    var id: Int { self.rawValue }
+    
+    case macOS
+    case linux
+    case `import`
+    
+    var imageName: String {
+        switch self {
+        case .macOS: return "macOS"
+        case .linux: return "Linux"
+        case .import: return "arrow.down.doc"
+        }
+    }
+    
+    var image: NSImage {
+        switch self {
+        case .macOS: return NSImage(named: "macOS")!
+        case .linux: return NSImage(named: "Linux")!
+        case .import: return NSImage(systemSymbolName: "arrow.down.doc", accessibilityDescription: nil)!
+        }
+    }
+    
+    var name: String {
+        switch self {
+        case .import: return "Import"
+        case .linux: return "Linux"
+        case .macOS: return "macOS"
+        }
+    }
+    
+    var restoreImageName: String {
+        switch self {
+        case .import: return ""
+        case .linux: return "ISO Image"
+        case .macOS: return "Restore Image"
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .import: return "Import an existing virtual machine"
+        case .linux: return "Install and run GUI Linux virtual machine"
+        case .macOS: return "Install and run macOS virtual machine"
+        }
+    }
+}
